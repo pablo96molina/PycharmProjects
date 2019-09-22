e13dacb68bcdb7874103a55eab325e9499027cd5
pablo96molina <54729876+pablo96molina@users.noreply.github.com>
2019-09-22 01:16:02 -0300

    Ultima version diario

--- Diario/Diary.py
+++ Diario/Diary.py
@@ -1,8 +1,8 @@
 from peewee import *
 import datetime
 import  sys
-
 from collections import OrderedDict
+
 db = SqliteDatabase("diary.db")
 
 
@@ -10,14 +10,16 @@ class Entry(Model):
 #Siempre heredar Model
     content = TextField()
     timestamp = DateTimeField(default=datetime.datetime.now)
-
     class Meta:
-    #Siempre poner la clase Meta conectando con db
         database = db
+    #Siempre poner la clase Meta conectando con db
+
 def create_and_conect():
     """Conecta la base de datos y crea las tablas"""
-    db.conect()
-    db.create_tables([Entry])
+    db.connect()
+    db.create_tables([Entry],safe=True)
+
+create_and_conect()
 def menu_loop():
     """Show Menu"""
     choice = None
@@ -42,22 +44,40 @@ def add_entry():
        Entry.create(content=data)
         #Carga en una linea el contenido de data
 
-def view_entries():
+def view_entries(search_query = None):
     """View all entries"""
-    entries = entries.select().order_by(entry.timestamp.desc())
+    entries = Entry.select().order_by(Entry.timestamp.desc())
+    if search_query:
+        entries = entries.where(Entry.content.contains(search_query))
+
     for entry in entries:
-        timestamp = entry.timestamp.strtime()
+        timestamp = entry.timestamp.strftime('%A %B %d, %Y %I:%M%p')
+        print (timestamp)
+        print ('+'*len(timestamp))
+        print (entry.content)
+        print("n) next entry")
+        print ("q) return to menu")
+        print ("d) delete entry")
 
-def delete_entry(Entry):
+        next_action = input("Action  ").lower().strip()
+        if next_action == 'q':
+            break
+        elif next_action == 'd':
+            delete_entry(entry)
+def search_entries():
+    """Search entries"""
+    search_query = input("Search query ").strip()
+    view_entries(search_query)
+def delete_entry(entry):
     """Delete entry"""
+    action = input("Are you sure? [Yn]").lower().strip()
 
-
-
-
-
+    if action == 'y':
+        entry.delete_instance()
 menu = OrderedDict([
     ('a', add_entry),
     ('v', view_entries),
+    ('s', search_entries),
     ('d', delete_entry)
 ])
 
