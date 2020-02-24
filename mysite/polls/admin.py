408d8173e5d4438a75e62ae0c40226fa32bfb47e
pablo96molina <54729876+pablo96molina@users.noreply.github.com>
2020-02-24 15:04:37 -0300

    2402

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-
 from django.contrib import admin
+from .models import Choice, Question
 
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -13,7 +13,5 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
-
-    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
