485a5ec4d21ec2c1c586e56128086f294b4ecb95
Mica <micalar27@gmail.com>
2020-02-28 16:50:36 -0300

    Se puede crear usuarios
    /signup para ver el form de registro

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+from home.forms import SignUpForm
 
 def home(request):
-    return render(request, 'encuestas/home.html')
+    return render(request, 'home/home.html')
 
 def signup(request):
-    return HttpResponse('<h1>Signup</h1>')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'home/signup.html', {'form': form})
