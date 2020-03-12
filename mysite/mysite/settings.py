37578aa2d7e063fb5de836801866065513d1e187
pablo96molina <54729876+pablo96molina@users.noreply.github.com>
2020-03-11 21:00:06 -0300

    Recuperar la ultima subid de Mica

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'polls',
-    'snipcartwagtaildemo',
 ]
 
 MIDDLEWARE = [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('snipcartwagtaildemo/', include('snipcartwagtaildemo.urls'))
 ]
