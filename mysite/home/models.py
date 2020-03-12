421b56383e2aecb517cafde5a7e13d0f70021906
Mica <micalar27@gmail.com>
2020-03-11 22:07:04 -0300

    Se recuperó la funcionalidad perdida

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -6,15 +6,15 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    saldo = models.IntegerField()
+    saldo = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user.username
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 # @receiver(post_save, sender=User)
 # def save_user_profile(sender, instance, **kwargs):
