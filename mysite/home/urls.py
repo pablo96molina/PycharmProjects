0de5e7e5228260eaa5cdd408c67ad96657b815db
Mica <micalar27@gmail.com>
2020-03-29 15:58:11 -0300

    Integrado nuevos templates del home y del login, pero no aparecen imagenes

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login, name='login'),
+    path('home/', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
 ]
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,39 +1,54 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout as auth_logout
 
 from home.forms import SignUpForm
 
-def home(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-        return redirect ('home')
+def login(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request.POST)
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('home')
+            return redirect ('login')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'polls/home.html', {'form': form})
     else:
-        form = AuthenticationForm()
-    return render(request, 'home/home.html', {'form': form})
+        return redirect('home')
+
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'polls/Dev-uy.html')
+    else:
+        return redirect('login')
+    
 
 def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                return redirect('home')
+        else:
+            form = SignUpForm()
+        return render(request, 'home/signup.html', {'form': form})
     else:
-        form = SignUpForm()
-    return render(request, 'home/signup.html', {'form': form})
+        return redirect('home')
 
 def logout(request):
     auth_logout(request)
-    return redirect('home')
+    return redirect('login')
 
