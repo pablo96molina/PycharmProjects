2d3f78c868162fa9243ecfb6508c55ce677dc439
pablo96molina <54729876+pablo96molina@users.noreply.github.com>
2020-04-17 22:16:56 -0300

    1707

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -98,7 +98,7 @@ WAGTAIL_SITE_NAME = 'My Example Site'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'basencuestas.sqlite3'),
     }
 }
 
--- mysite/snipcartwagtaildemo/snipcartwagtaildemo/settings/base.py
+++ mysite/snipcartwagtaildemo/snipcartwagtaildemo/settings/base.py
@@ -95,7 +95,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'basencuestas': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'basencuestas.sqlite3',
+    } 
 }
 
 
